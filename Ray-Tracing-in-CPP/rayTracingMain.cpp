3f70060bff1a920dc41091fc0239c857ba10bdc5
Ripple Domain <mfurkandemir554223@gmail.com>
2025-05-15 03:01:24 +0300

    Created the main window and initialized a white square on the window.

--- Ray-Tracing-in-CPP/rayTracingMain.cpp
+++ Ray-Tracing-in-CPP/rayTracingMain.cpp
@@ -7,7 +7,19 @@ int main()
 	static const int HEIGHT = 600;
 
 	SDL_Init(SDL_INIT_VIDEO);
-	SDL_Window* window = SDL_CreateWindow("Ray Tracing", SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT);
+
+	SDL_Window* window = SDL_CreateWindow("Ray Tracing", WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, nullptr);
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+	SDL_RenderClear(renderer);
+
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_FRect rect = {100, 100, 200, 200};
+	SDL_RenderFillRect(renderer, &rect);
+
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(5000);
 
 	return 0;
 }
