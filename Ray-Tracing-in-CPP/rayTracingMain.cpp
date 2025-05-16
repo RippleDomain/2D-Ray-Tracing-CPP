e28eed544b6ef7b215acd7a78e3510ad86a9f6c9
Ripple Domain <mfurkandemir554223@gmail.com>
2025-05-16 03:22:26 +0300

    Created a Circle object and rendered it on the window.

--- Ray-Tracing-in-CPP/rayTracingMain.cpp
+++ Ray-Tracing-in-CPP/rayTracingMain.cpp
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <SDL3/SDL.h>
 
+struct Circle
+{
+	float x, y, r;
+};
+
+void fillCircle(SDL_Renderer* renderer, Circle circle)
+{
+	float x = circle.r;
+	float y = 0.0f;
+	float err = 0.0f;
+
+	while (x >= y)
+	{
+		SDL_RenderLine(renderer, circle.x - x, circle.y + y, circle.x + x, circle.y + y);
+		SDL_RenderLine(renderer, circle.x - y, circle.y + x, circle.x + y, circle.y - x);
+		SDL_RenderLine(renderer, circle.x - x, circle.y - y, circle.x + x, circle.y - y);
+		SDL_RenderLine(renderer, circle.x - y, circle.y - x, circle.x + y, circle.y + x);
+
+		if (err <= 0.0f)
+		{
+			y += 1.0f;
+			err += 2.0f * y + 1.0f;
+		}
+		if (err > 0.0f)
+		{
+			x -= 1.0f;
+			err -= 2.0f * x + 1.0f;
+		}
+	}
+}
+
 int main() 
 {
 	static const int WIDTH = 800;
@@ -14,8 +45,8 @@ int main()
 	SDL_RenderClear(renderer);
 
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_FRect rect = {100, 100, 200, 200};
-	SDL_RenderFillRect(renderer, &rect);
+	Circle circle = {400, 300, 100};
+	fillCircle(renderer, circle);
 
 	SDL_RenderPresent(renderer);
 
